7f2954b87fddce1b8e82b69da8017a0c9c13542c
wagnersenger <wagnersenger@gmail.com>
2015-10-05 14:41:06 -0300

    Login

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,5 @@
-var _HOST_URL = 'http://mobile.btrac.com.br/';
-//var _HOST_URL = 'http://192.168.146.128:8083/';
+//var _HOST_URL = 'http://mobile.btrac.com.br/';
+var _HOST_URL = 'http://192.168.146.128:8083/';
 var _CURRENT_VERSION = '1.0.4';
 var _GMAPS_KEY = 'AIzaSyABBAEz0olIUCgvbZlCSuAPwkq8brNQXd8';
 var _GMAPS_ADDRESS_KEY = 'AIzaSyDf9xnK5US5l1xwrD66IJPauQrsAYrv25k';
